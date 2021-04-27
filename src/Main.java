906898cda7dde0db7844c4534a3822712338548f
rnade046 <rnade046@uottawa.ca>
2021-04-27 08:55:02 -0400

    Update file to check protein freq in all annotation files

--- src/Main.java
+++ src/Main.java
@@ -20,46 +20,46 @@ public class Main {
 		String wd = "C:\\Users\\Rachel\\Documents\\LESMoNlocal\\";
 
 		String mapProtToRefSeqFile = wd + "motif_enumeration\\BiomaRt_MappingRefSeqIdsToGeneSymbol_corrNet.tsv";
-		
+
 		String correlationRepository = wd + "input_files\\correlation_v2.tsv";
 		String proteinsInNetwork = wd + "CorrelationNet_listOfProteinNames.tsv";
 		String distanceMatrixFile = wd + "IO_files\\distanceMatrix_cellMap_CorrNet.txt";
-		
+
 		String fastaFile = wd + "input_files\\human_3UTRsequences.txt";
 		String annotationFile = wd + "IO_files\\motifMapped.tsv";
 		String proteinAnnotationFrequencyFile = wd + "IO_files\\test-protFreqAnnotation.tsv";
 		String mcSamplingFile = wd + "IO_files\\test-MonteCarloSamplingFile_n3_s100000.tsv";
-		
+
 		System.out.println("**Loading interaction repository**");
 		ArrayList<Interaction> interactionList = CorrelationGraphLoader.loadGraphFromCorrelationNetwork(correlationRepository, fastaFile, mapProtToRefSeqFile, proteinsInNetwork, 0.5);
 		System.out.println("Number of interactions:" + interactionList.size() + "\n");
-		
+
 		System.out.println("**Getting list of proteins in network**");
 		ArrayList<Protein> proteinList = NetworkProteins.getProteinsInNetwork(interactionList);
 		System.out.println("Number of Proteins: " + proteinList.size() + "\n");
 		//printProtAndRefSeqIdsInNetwork(rnaIdListFile, proteinList);
 		//printRefSeqIdsInNetwork(rnaIdListFile, proteinList);
-		
+
 		File f = new File(distanceMatrixFile);
 		if(!f.exists() && !f.isDirectory()) {
 			System.out.println("**Generating distance matrix**");
 			DistanceMatrix.computeDistanceMatrix(interactionList, proteinList, distanceMatrixFile);
 		}
-		
+
 		double[][] distanceMatrix = DistanceMatrix.loadDistanceMatrix(distanceMatrixFile, proteinList); 
-		
+
 		if(mcSampling) {
-		// For MC sampling 
-		// 1 - Make list: protein = #motifs (degen + non degen) from full annotation list
-		File f1 = new File(proteinAnnotationFrequencyFile);
-		if(!f1.exists() && !f1.isDirectory()) {
-			System.out.println("Enumerating protein annotation frequency file");
-			ProteinAnnotations.enumerateProteinAnnotationFrequency(annotationFile, proteinAnnotationFrequencyFile);
-		}
-		// 2 - Initialize sampling
-		MotifSampling sampling = new MotifSampling(proteinAnnotationFrequencyFile, proteinList, distanceMatrix);
-		// 3 - Perform sampling for n proteins
-		sampling.computeMultipleDistributions(3, 3, 10000, mcSamplingFile);
+			// For MC sampling 
+			// 1 - Make list: protein = #motifs (degen + non degen) from full annotation list
+			File f1 = new File(proteinAnnotationFrequencyFile);
+			if(!f1.exists() && !f1.isDirectory()) {
+				System.out.println("Enumerating protein annotation frequency file");
+				ProteinAnnotations.enumerateProteinAnnotationFrequency(annotationFile, proteinAnnotationFrequencyFile);
+			}
+			// 2 - Initialize sampling
+			MotifSampling sampling = new MotifSampling(proteinAnnotationFrequencyFile, proteinList, distanceMatrix);
+			// 3 - Perform sampling for n proteins
+			sampling.computeMultipleDistributions(3, 3, 10000, mcSamplingFile);
 		}
 	}
 
@@ -71,7 +71,7 @@ public class Main {
 			for(Protein prot: proteinList) {
 
 				if(prot.getProteinId() != null) {
-					
+
 					for(String rnaId: prot.getProteinId()) {
 						out.write(rnaId + "\n");
 						out.flush();
@@ -95,7 +95,7 @@ public class Main {
 
 				if(prot.getProteinId() != null) {
 					out.write(prot.getProteinName() + "\t");
-					
+
 					for(String rnaId: prot.getProteinId()) {
 						out.write(rnaId + "|");
 					}
--- src/opt/checkAnnotations.java
+++ src/opt/checkAnnotations.java
@@ -14,66 +14,120 @@ public class checkAnnotations {
 
 	public static void main(String[] args) {
 
+		String inputFiles = "mapDegenMotifsToProteins_";
+		String regMotifFiles = "";
 		String outputFile = "";
-		
+
 		int numFiles = 992;
-		HashMap<Integer,Integer> mapOfProteinFreq = calculateProteinFreq(numFiles);
+		HashMap<Integer,Integer> mapOfProteinFreq = new HashMap<>();
+		
+		calculateProteinFreqFromDegenMotifs(mapOfProteinFreq, inputFiles, numFiles);
+		calculateProteinFreqFromRegularMotifs(mapOfProteinFreq, regMotifFiles);
+		
 		outputFreqOfProteinMap(mapOfProteinFreq, outputFile);
 
 	}
 
-	private static HashMap<Integer, Integer> calculateProteinFreq(int numFiles) {
+	/**
+	 * Iterate through all annotation files (degen motifs) to calculate the occurrence of 
+	 * a motif annotation X number of proteins 
+	 * 
+	 * @param mapOfProteinFreq	HashMap<Integer, Integer> - to contain number of annotated proteins = occurrence 
+	 * @param inputFiles		String - file path with annotation file prefix
+	 * @param numFiles			int - number of degenMotifs files (corresponds to the file labeling)
+	 */
+	private static void calculateProteinFreqFromDegenMotifs(HashMap<Integer, Integer> mapOfProteinFreq, String inputFiles, int numFiles) {
 
-		HashMap<Integer, Integer> mapOfProteinFreq = new HashMap<>();
-
+		/* iterate over all degen motif annotation files */
 		for(int i=0; i < numFiles; i++) {
 
-			String annotationFile = "mapDegenMotifsToProteins_" + i + ".tsv"; 
+			String annotationFile = inputFiles + i; // current annotation file (ex. mapDegenMotifsToProteins_200) 
 
 			InputStream in;
 			try {
 				in = new FileInputStream(new File(annotationFile));
 				BufferedReader input = new BufferedReader(new InputStreamReader(in));
-				
-				String line = input.readLine();
-				
+
+				String line = input.readLine(); // no header
+
 				while(line!=null) {
-					
-					Integer numOfProt = Integer.parseInt(line.split("\t")[2]);
-					
-					if(mapOfProteinFreq.containsKey(numOfProt)) {
+
+					Integer numOfProt = Integer.parseInt(line.split("\t")[2]); // number of proteins that contain this motif
+
+					/* Update map */
+					if(mapOfProteinFreq.containsKey(numOfProt)) {	
 						mapOfProteinFreq.put(numOfProt, mapOfProteinFreq.get(numOfProt) + 1);
 					} else {
 						mapOfProteinFreq.put(numOfProt, 1);
 					}
-					
+
 					line = input.readLine();
 				}
-				
+
 				input.close();
 			} catch(IOException e) {
 				e.printStackTrace();
 			}
 		}
-
-		return mapOfProteinFreq;
 	}
-	
+
+	/**
+	 * Iterate through all annotation files (degen motifs) to calculate the occurrence of 
+	 * a motif annotation X number of proteins 
+	 * 
+	 * @param mapOfProteinFreq	HashMap<Integer, Integer> - to contain number of annotated proteins = occurrence 
+	 * @param inputFiles		String - file path with annotation file prefix
+	 * @param numFiles			int - number of degenMotifs files (corresponds to the file labeling)
+	 */
+	private static void calculateProteinFreqFromRegularMotifs(HashMap<Integer, Integer> mapOfProteinFreq, String inputFile) {
+
+		InputStream in;
+		try {
+			in = new FileInputStream(new File(inputFile));
+			BufferedReader input = new BufferedReader(new InputStreamReader(in));
+
+			String line = input.readLine(); // no header
+
+			while(line!=null) {
+
+				Integer numOfProt = Integer.parseInt(line.split("\t")[2]); // number of proteins that contain this motif
+
+				/* Update map */
+				if(mapOfProteinFreq.containsKey(numOfProt)) {	
+					mapOfProteinFreq.put(numOfProt, mapOfProteinFreq.get(numOfProt) + 1);
+				} else {
+					mapOfProteinFreq.put(numOfProt, 1);
+				}
+
+				line = input.readLine();
+			}
+
+			input.close();
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	/**
+	 * Print the number of annotated proteins to occurrence map
+	 * 
+	 * @param mapOfProteinFreq	HashMap<Integer, Integer> number of proteins annotated = occurrence in annotation file
+	 * @param outputFile		String - file path
+	 */
 	private static void outputFreqOfProteinMap(HashMap<Integer, Integer> mapOfProteinFreq, String outputFile) {
-		
+
 		try {
 			BufferedWriter out = new BufferedWriter(new FileWriter(new File(outputFile)));
-			
+
 			for(int numProt: mapOfProteinFreq.keySet()) {
 				out.write(numProt + "\t" + mapOfProteinFreq.get(numProt) + "\n");
 				out.flush();
 			}
-			
+
 			out.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
 
 }
