9aa417ba0dc3ffbc5aac81fc34fb01b1ebf242da
rnade046 <rnade046@uottawa.ca>
2021-04-29 09:46:52 -0400

    Implement properties file

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Properties;
 
 import graph.Interaction;
 import graph.Protein;
@@ -15,23 +18,30 @@ import utils.NetworkProteins;
 
 public class Main {
 
-	public static void main(String[] args) {
-		boolean mcSampling = false;
-		String wd = "C:\\Users\\Rachel\\Documents\\LESMoNlocal\\";
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		
+		System.out.println("**Loading parameters file** \n");
+		Properties params = new Properties();
+		params.load(new FileInputStream(args[0]));		
+		
+		String wd = params.getProperty("working_directory");
 
-		String mapProtToRefSeqFile = wd + "motif_enumeration\\BiomaRt_MappingRefSeqIdsToGeneSymbol_corrNet.tsv";
+		String fastaFile = wd + params.getProperty("fastaFile");
+		String mapProtToRefSeqFile = wd + params.getProperty("mapGeneSymbolsToRefSeqIds");
 
-		String correlationRepository = wd + "input_files\\correlation_v2.tsv";
-		String proteinsInNetwork = wd + "CorrelationNet_listOfProteinNames.tsv";
-		String distanceMatrixFile = wd + "IO_files\\distanceMatrix_cellMap_CorrNet.txt";
+		String correlationRepository = wd + params.getProperty("networkRepositoryFile");
+		String proteinsInNetworkOutputFile = wd + "CorrelationNet_listOfProteinNames.tsv";
 
-		String fastaFile = wd + "input_files\\human_3UTRsequences.txt";
-		String annotationFile = wd + "IO_files\\motifMapped.tsv";
-		String proteinAnnotationFrequencyFile = wd + "IO_files\\test-protFreqAnnotation.tsv";
-		String mcSamplingFile = wd + "IO_files\\test-MonteCarloSamplingFile_n3_s100000.tsv";
+		String distanceMatrixFile = wd + params.getProperty("distanceMatrixFile");
+
+		String annotationFile = wd + params.getProperty("motifAnnotationFile");
+		String degenAnnotationPrefix = wd + params.getProperty("degenAnnotationPrefix");
+		
+		String proteinAnnotationFrequencyFile = wd + params.getProperty("proteinAnnotationFrequencyFile");
+		String mcSamplingPrefix = wd + params.getProperty("mcSamplingPrefix");
 
 		System.out.println("**Loading interaction repository**");
-		ArrayList<Interaction> interactionList = CorrelationGraphLoader.loadGraphFromCorrelationNetwork(correlationRepository, fastaFile, mapProtToRefSeqFile, proteinsInNetwork, 0.5);
+		ArrayList<Interaction> interactionList = CorrelationGraphLoader.loadGraphFromCorrelationNetwork(correlationRepository, fastaFile, mapProtToRefSeqFile, proteinsInNetworkOutputFile, 0.5);
 		System.out.println("Number of interactions:" + interactionList.size() + "\n");
 
 		System.out.println("**Getting list of proteins in network**");
@@ -39,20 +49,20 @@ public class Main {
 		System.out.println("Number of Proteins: " + proteinList.size() + "\n");
 		//printProtAndRefSeqIdsInNetwork(rnaIdListFile, proteinList);
 		//printRefSeqIdsInNetwork(rnaIdListFile, proteinList);
-		
+
 		File f = new File(distanceMatrixFile);
 		if(!f.exists() && !f.isDirectory()) {
 			System.out.println("**Generating distance matrix**");
 			DistanceMatrix.computeDistanceMatrix(interactionList, proteinList, distanceMatrixFile);
 		}
-		
+
 		/* Perform motif enumeration around here */
-		
+
 		System.out.println("**Loading distance matrix**");
 		double[][] distanceMatrix = DistanceMatrix.loadDistanceMatrix(distanceMatrixFile, proteinList); 
 
-		if(mcSampling) {
-			
+
+		if(Boolean.parseBoolean(params.getProperty("calculateProteinAnnotationFreq"))) {	
 			// For MC sampling 
 			// 1 - Make list: protein = #motifs (degen + non degen) from full annotation list	>> Do this once
 			File f1 = new File(proteinAnnotationFrequencyFile);
@@ -60,16 +70,17 @@ public class Main {
 				System.out.println("Enumerating protein annotation frequency file");
 				ProteinAnnotations.enumerateProteinAnnotationFrequency(annotationFile, proteinAnnotationFrequencyFile);
 			}
-			
+		}
+		if(Boolean.parseBoolean(params.getProperty("performMCprocedure"))) {
 			/* This will be done in job arrays on CC */
 			System.out.println("**Performing Monte Carlo Sampling Procedure**");
 			// 2 - Initialize sampling
 			MotifSampling sampling = new MotifSampling(proteinAnnotationFrequencyFile, proteinList, distanceMatrix);
 			// 3 - Perform sampling for n proteins
-			sampling.computeMultipleDistributions(3, 3, 10000, mcSamplingFile);
+			sampling.computeMultipleDistributions(3, 3, 10000, mcSamplingPrefix);
 		}
-		
-		
+
+
 	}
 
 	public static void printRefSeqIdsInNetwork(String outputFile, ArrayList<Protein> proteinList) {
