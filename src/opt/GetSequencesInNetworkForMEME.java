e17eecdf71d31d4ae7f0cd517eac10e8844c73d2
rnade046 <rnade046@uottawa.ca>
2022-03-03 11:39:56 -0500

    Print only longest 3'UTR sequence for MEME analysis

--- src/opt/GetSequencesInNetworkForMEME.java
+++ src/opt/GetSequencesInNetworkForMEME.java
@@ -8,9 +8,11 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 
 public class GetSequencesInNetworkForMEME {
 
@@ -22,7 +24,7 @@ public class GetSequencesInNetworkForMEME {
 
 		String mclOutputFile = wd + "benchmark//mclOutput_i2.txt";
 
-		// Load MCL output file, line-by-line (one line = one cluster)
+		/* Load MCL output file, line-by-line (one line = one cluster) */ 
 		System.out.println("**Get sequences for MEME analysis**");
 		try {
 			InputStream in = new FileInputStream(new File(mclOutputFile));
@@ -32,15 +34,25 @@ public class GetSequencesInNetworkForMEME {
 			int lineCount = 1;
 			while(line !=null) {
 				System.out.println(lineCount);
-				HashSet<String> proteins = new HashSet<>(Arrays.asList(line.split("\t")));
+				HashSet<String> proteins = new HashSet<>(Arrays.asList(line.split("\t"))); // set of proteins in MCL cluster
+				System.out.println("cluster contains x proteins : " + proteins.size());
 				if(proteins.size() >= 3) {
-					// Load refSeqIds associated to proteins in cluster
-					HashMap<String, Boolean> refSeqIdSet = loadRefSeqIdsFromNetworkInfoFile(proteins, proteinInfoFile);
+					/* Load refSeqIds associated to proteins in cluster */
+					List<HashSet<String>> refSeqIDs = getRefSeqIdsCorrespondingToProteinsFromNetworkInfoFile(proteins, proteinInfoFile);
+					System.out.println("Found corresponding refseqIds for x proteins : " + refSeqIDs.size());
+					
+					// Determine sequences to print
+					System.out.println("Selecting refSeqIds to use:");
+					HashMap<String, Boolean	> idsToPrintMap = selectSequencesToPrint(fastaFile, refSeqIDs);
 
-					// Iterate through FASTA file, printing lines if corresponds to refSeqId in network
-					String outputFile = wd + "benchmark//fasta//corrNet2-400_sequences_fasta_MEME_mclCluster_"+ lineCount+".txt";
-
-					printSequencesInNetwork(fastaFile, refSeqIdSet, outputFile);
+					if(idsToPrintMap.size() >= 3) {
+						// Iterate through FASTA file, printing lines if corresponds to refSeqId in network
+						String outputFile = wd + "benchmark//fasta//corrNet2-400_sequences_fasta_MEME_mclCluster_"+ lineCount+".txt";
+						
+						System.out.println("Printing sequences: " + idsToPrintMap.size());
+						printSequencesInNetwork(fastaFile, idsToPrintMap, outputFile);
+						System.out.println("Done");
+					}
 				}
 				line = input.readLine();
 				lineCount++;
@@ -53,9 +65,17 @@ public class GetSequencesInNetworkForMEME {
 
 	}
 
-	private static HashMap<String, Boolean> loadRefSeqIdsFromNetworkInfoFile(HashSet<String> proteinsInCluster, String networkInfoFile){
+	/**
+	 * Obtain the refSeqIDs corresponding to the different proteins in the given MCL cluster
+	 * These IDs are used in the LESMoN approach.
+	 * 
+	 * @param proteinsInCluster Set<String> - Set of proteins
+	 * @param networkInfoFile	String - File containing proteinName \t RefSeqId1|2|..|n
+	 * @return refSeqIDs		List<String[]> - List of [RefSeqId1, 2, .., n] 
+	 */
+	private static List<HashSet<String>> getRefSeqIdsCorrespondingToProteinsFromNetworkInfoFile(HashSet<String> proteinsInCluster, String networkInfoFile){
 
-		HashMap<String, Boolean> refSeqIDSet = new HashMap<>();
+		List<HashSet<String>> refSeqIDList= new ArrayList<HashSet<String>>();
 
 		try {
 			InputStream in = new FileInputStream(new File(networkInfoFile));
@@ -64,13 +84,11 @@ public class GetSequencesInNetworkForMEME {
 			String line = input.readLine();
 			while(line !=null) {
 
-				if(proteinsInCluster.contains(line.split("\t")[0])) {
-					if(line.split("\t").length > 1) {
-						String[] refSeqIds = line.split("\t")[1].split("\\|");
-						for(String id : refSeqIds) {
-							refSeqIDSet.put(id, true);
-						}
+				if(proteinsInCluster.contains(line.split("\t")[0])) { // [0] = proteinName
 
+					if(line.split("\t").length > 1) {  // check if protein has corresponding refSeqIds (possible there are none)
+						String[] refSeqIds = line.split("\t")[1].split("\\|");
+						refSeqIDList.add(new HashSet<String>(Arrays.asList(refSeqIds)));
 					}
 				}
 				line = input.readLine();
@@ -80,7 +98,61 @@ public class GetSequencesInNetworkForMEME {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		return refSeqIDSet;
+		return refSeqIDList;
+	}
+
+	public static HashMap<String, Boolean> selectSequencesToPrint(String fastaFile, List<HashSet<String>> refSeqIds){
+		HashMap<String, Boolean> idsToPrintSet = new HashMap<>();
+		
+		int idCount = 0;
+		for(HashSet<String> idSet : refSeqIds) { // IDs correspond to a single protein
+			
+			if(idSet.size() > 1) {
+				/* Check if sequence exists corresponding to Id; and get sequence length */
+				int maxSequenceLength=0;
+				String idToPrint = "";
+
+				try {
+					InputStream in = new FileInputStream(new File(fastaFile));
+					BufferedReader input = new BufferedReader(new InputStreamReader(in));
+
+					String line = input.readLine();
+					int sequenceCount = 0; 
+
+					while(line != null || sequenceCount < idSet.size()) {
+
+						if(line.startsWith(">")) {
+							String id = line.split("\\_")[2] + "_" + line.split("\\_|\\.")[3];
+							if(idSet.contains(id)) {
+								sequenceCount ++; // sequence has been found
+
+								/* calculate sequence length */ 
+								String[] positions = line.split(":| |-");
+								int seqLength = Integer.parseInt(positions[3]) - Integer.parseInt(positions[2]) + 1;
+
+								if(seqLength > maxSequenceLength) {
+									maxSequenceLength = seqLength;
+									idToPrint = id;
+								}
+							}
+						}
+						line = input.readLine();	
+					}
+					input.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				idsToPrintSet.put(idToPrint, true);
+			} else {
+				for(String id: idSet) {
+					idsToPrintSet.put(id, true);
+				}	
+			}
+			idCount++;
+			System.out.print(idCount + ".");
+			
+		}
+		return idsToPrintSet;
 	}
 
 	public static void printSequencesInNetwork(String fastaFile, HashMap<String, Boolean> refSeqIdSet, String outputFile) {
