7a2cb103764f517f7c8c814bda8744117d736849
rnade046 <rnade046@uottawa.ca>
2022-03-10 13:56:12 -0500

    Output significant MEME motifs

--- src/benchmark/FilterMemeOutput.java
+++ src/benchmark/FilterMemeOutput.java
@@ -16,61 +16,57 @@ public class FilterMemeOutput {
 
 		String memeInputFilePrefix = "C:\\Users\\Rachel\\Documents\\LESMoNlocal\\analysis\\benchmark\\meme-zoops-results-2\\meme_cluster_";
 		String memeFilteredFilePrefix = "C:\\Users\\Rachel\\Documents\\LESMoNlocal\\analysis\\benchmark\\meme-zoops-results-2\\memeFiltered_cluster_";
-		
+
 		String summaryFile = "C:\\Users\\Rachel\\Documents\\LESMoNlocal\\analysis\\benchmark\\meme-zoops-summaryFile.tsv";
 		int numOfFilesToFilter = 104;
 		double threshold = 0.05;
-		
+
 		HashMap<Integer, Integer> significantMotifsMap = new HashMap<>();
-		
+
 		/* filter every MEME.txt file output by MEME analysis on MCL clusters */
 		for(int i=1; i<= numOfFilesToFilter; i++) {
 			System.out.println("File " + i);
-			
+
 			/* check if file exits */
 			File f = new File(memeInputFilePrefix + i + ".txt");
 			if(f.exists() && !f.isDirectory()) { 
-				
+
 				int motifsTotal = 0;
 				int motifsKept = 0;
-				
+
 				try {
 					BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(new File(memeInputFilePrefix + i + ".txt"))));
 					BufferedWriter out = new BufferedWriter(new FileWriter(new File(memeFilteredFilePrefix + i + ".txt")));
 
+					/* Header */
+					out.write("MEME version 4\n\n" + "ALPHABET= ACGT\n\n" + 
+							"Background letter frequencies\nA 0.25 C 0.25 G 0.25 T 0.25\n\n"); // header
+
 					String line = in.readLine();
-					String stars = line;
 
-					boolean print = true;
-
-					boolean header = false;
-
-					int starCount=0;
+					boolean printMotif = false;
+					String motif="";
+					int starCount = 0;
 					while(line != null) {
 
 						if(line.startsWith("*")) {
+
 							starCount++;
 
 							switch(starCount) {
 							case 1: 			// start of header 
-								header = true;
-								print = true;
-
 								line = in.readLine(); // next line
-
 								if(line.startsWith("MOTIF")) {
 									motifsTotal++;
-
-									/* check e-val*/
-									if(Double.parseDouble(line.split("\\s+")[14]) > threshold) {
-										print = false;
-									} else {
+									/* check E-val*/
+									if(Double.parseDouble(line.split("\\s+")[14]) < threshold) {
+										motif = line.split("\\s+")[2];
+										printMotif = true;
 										motifsKept++;
+										
+										starCount++;
 									}
 								}
-								break;
-							case 2 : 			// end of header
-								header = false;
 								break;
 							case 3: 			// end of sections
 								starCount = 0; 
@@ -79,16 +75,33 @@ public class FilterMemeOutput {
 						}
 
 						/* print lines */
-						if(print) {
-							if(header) {
-								out.write(stars + "\n");
+						if(printMotif) {
+							boolean ppmFound = false;
+
+							while(!ppmFound) {
+
+								if(line.contains("position-specific probability matrix")) {
+									ppmFound = true;
+
+									out.write("MOTIF " + motif + "\n");
+									line = in.readLine();
+								}
+								line = in.readLine();
 							}
-							out.write(line +"\n");
+
+							while (!line.contains("---")) {
+								out.write(line + "\n");
+								out.flush();
+								line = in.readLine();
+							}
+							
+							out.write("\n");
+							printMotif=false; 
 						}
 						line = in.readLine();
 					}
 					System.out.println("Motifs kept : " + motifsKept + " / " + motifsTotal);
-					
+
 					significantMotifsMap.put(i, motifsKept);
 					out.close();
 					in.close();
@@ -101,20 +114,20 @@ public class FilterMemeOutput {
 			}
 		}
 		printMEMEmotifSummary(summaryFile, significantMotifsMap);
-	
+
 	}
-	
+
 	private static void printMEMEmotifSummary(String outputFile, HashMap<Integer, Integer> significantMotifMap) {
-		
+
 		try {
 			BufferedWriter out = new BufferedWriter(new FileWriter(new File(outputFile)));
-			
+
 			out.write("Cluster\tSignificantMotifs\n"); 
-			
+
 			for(Entry<Integer, Integer> e : significantMotifMap.entrySet()) {
 				out.write(e.getKey() + "\t" + e.getValue() + "\n");
 			}
-			
+
 			out.close();
 		} catch (IOException e) {
 			e.printStackTrace();
