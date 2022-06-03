64644381a86d38d0784b87925176773cd4e0f387
rnade046 <39959339+rnade046@users.noreply.github.com>
2022-06-03 08:46:42 -0400

    generate reverse complements for all sequences

--- post/positionConservation/ReverseComplement.java
+++ post/positionConservation/ReverseComplement.java
@@ -8,36 +8,133 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.HashSet;
 
 public class ReverseComplement {
 
 	public static void main(String[] args) {
-		
-		String inputFile = "C:\\Users\\rnade046\\Documents\\LESMoNlocal\\analysis\\corrNetTop2-400_coreTPD_p0.4_coreProteins_h0.7_motifFamiliesInfo.tsv";
-		String outputFile = "C:\\Users\\rnade046\\Documents\\LESMoNlocal\\analysis\\MotifPosition\\corrNetTop2-400_coreTPD_p0.4_coreProteins_reverse-complement_motifs.txt";
-	
-		listReverseComplementMotifs(inputFile, outputFile);
+
+		String inputFile ="C:\\Users\\rnade046\\Documents\\LESMoNlocal\\analysis\\MotifPosition\\corrNetTop2_longestSequence.tsv";
+		String fastaFile = "C:\\Users\\rnade046\\Documents\\LESMoNlocal\\analysis\\MotifPosition\\coding-sequences-human.txt";
+		String outputFile = "C:\\Users\\rnade046\\Documents\\LESMoNlocal\\analysis\\MotifPosition\\corrNetTop2_reverse-complement-sequences.txt";
+
+		generateReverseComplements(inputFile, fastaFile, outputFile);
+		//listReverseComplementMotifs(inputFile, outputFile);
 	}
 
+	private static void generateReverseComplements(String refSeqIdsFile, String fastaFile, String outputFile) {
+
+		/* obtain refseq of sequences to consider */
+		HashSet<String> refSeqIdSet = getRefSeqIdSet(refSeqIdsFile);
+		System.out.println("Sequences to find: " + refSeqIdSet.size());
+
+		/* load sequences associated to ids in the fasta file */
+		try {
+			BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(new File(fastaFile))));
+			BufferedWriter out = new BufferedWriter(new FileWriter(new File(outputFile)));
+			
+			String line = in.readLine(); // no header 
+			
+			boolean storeSeq = false;
+			String seq = "";
+			String id = "";
+			
+			int motifsFound = 0;
+			
+			while(line!=null) {
+				
+				if(storeSeq & !line.startsWith(">")) {
+					seq += line;
+				}
+				
+				if(line.startsWith(">")) { // new entry, check id
+					
+					/* if a sequence is loaded */
+					if(!seq.isEmpty()) {
+						
+						/* generate reverse complement */
+						String revC = convertSequence(seq);
+						
+						/* output reverse complement */
+						out.write(">RevComplement_" + id + "\n");
+						out.write(revC + "\n");
+						out.flush();
+						
+						/* re-initialize variables */
+						seq = "";
+						id = "";
+						storeSeq = false;
+						
+						if(motifsFound == refSeqIdSet.size()) {
+							break;
+						}
+					}
+					
+					/* get current id */
+					String[] col = line.split("_|\\.");
+					id = col[2] + "_" + col[3]; // col[2] = type of ID (eg. NM) ; col[3] = number ID (#####)
+
+					/* if id is in our list, store it's sequence to obtain it's reverse complement */
+					if(refSeqIdSet.contains(id)) {
+						storeSeq = true;
+						motifsFound++;
+						
+						System.out.print(motifsFound + ".");
+						
+						if(motifsFound%50 == 0) {
+							System.out.println();
+						}
+					}	
+				}
+				line = in.readLine();
+			}
+	
+			in.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static HashSet<String> getRefSeqIdSet(String inputFile){
+
+		HashSet<String> refSeqIdSet = new HashSet<>();
+		try {
+			BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(new File(inputFile))));
+
+			String line = in.readLine(); // no header
+			while(line != null) {
+
+				refSeqIdSet.add(line.split("\t")[1]); // [1] = refSeqId
+				line = in.readLine();
+			}
+			in.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return refSeqIdSet;
+	}
+
+
+	@SuppressWarnings("unused")
 	private static void listReverseComplementMotifs(String inputFile, String outputFile) {
 
 		InputStream in;
 		try {
 			in = new FileInputStream(new File(inputFile));
 			BufferedReader input = new BufferedReader(new InputStreamReader(in));
-			
+
 			BufferedWriter out = new BufferedWriter(new FileWriter(new File(outputFile)));
-			
+
 			String line = input.readLine();
 			line = input.readLine();
 			int count = 1;
 			out.write("Family\tReverse-Complement\n");
-			
+
 			while(line!=null) {
-				
+
 				String motif = line.split("\t")[1];
-				out.write(count + "\t" + convertMotif(motif) + "\n");
-				
+				out.write(count + "\t" + convertSequence(motif) + "\n");
+
 				line = input.readLine();
 				count++;
 			}
@@ -47,25 +144,25 @@ public class ReverseComplement {
 			e.printStackTrace();
 		}
 	}
-	
-	private static String convertMotif(String motif) {
+
+	private static String convertSequence(String motif) {
 		String finalMotif = "";
-		
+
 		String complement = "";
 		for(int i=0; i<motif.length(); i++) {
 			complement += mapComplement(motif.charAt(i));
 		}
-		
+
 		for(int i=motif.length()-1; i>=0; i--) {
 			finalMotif += complement.charAt(i);
 		}
-		
+
 		return finalMotif;
 	}
 
 	private static char mapComplement(char c) {
 
-		char n = 'X';
+		char n = ' ';
 
 		switch(c) {
 		case 'A' : n = 'T';
