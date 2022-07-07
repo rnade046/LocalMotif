0782e22ff112c1e748b6604eeb7e604dda21fde9
rnade046 <39959339+rnade046@users.noreply.github.com>
2022-07-07 11:46:51 -0400

    Update paths for coding sequence

--- src/randomSeq/RandomMain.java
+++ src/randomSeq/RandomMain.java
@@ -4,11 +4,15 @@ public class RandomMain {
 	
 	public static void main(String[] args) {
 		
-		String wd = "C:\\Users\\rnade046\\Documents\\LESMoNlocal\\analysis\\";
+		String wd = "/Users/rnadeau2/Documents/LESMoNlocal/analysis/";
 		
-		String fastaFile = wd + "input_files\\human_3UTRsequences.txt";
-		String rnaIdListFile = wd + "motif_enumeration\\BiomaRt_MappingRefSeqIdsToGeneSymbol_corrNet.tsv";
-		String randomFastaFile = wd + "input_files\\random_humanCellMap_3UTRsequences.txt";
+//		String fastaFile = wd + "input_files\\human_3UTRsequences.txt";
+//		String rnaIdListFile = wd + "motif_enumeration\\BiomaRt_MappingRefSeqIdsToGeneSymbol_corrNet.tsv";
+//		String randomFastaFile = wd + "input_files\\random_humanCellMap_3UTRsequences.txt";
+		
+		String fastaFile = wd + "MotifPosition/coding-sequences-human.txt";
+		String rnaIdListFile = wd + "MotifPosition/corrNetTop2_longestSequence.tsv";
+		String randomFastaFile = wd + "MotifPosition/LocalRandom_CodingSequences.txt";
 		
 		System.out.println("**Generating randomized fasta sequences**");
 		RandomizeSequences.generateRandomizedFasta(rnaIdListFile, fastaFile, randomFastaFile);
