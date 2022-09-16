fbb0c37114b0b581eca84c7c25f67f1b3922c65d
rnade046 <39959339+rnade046@users.noreply.github.com>
2022-09-16 09:42:17 -0400

    Get positions from core genes

--- post/evolutionConservation/DetermineMotifGenomePostions.java
+++ post/evolutionConservation/DetermineMotifGenomePostions.java
@@ -12,6 +12,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Random;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -21,14 +22,21 @@ public class DetermineMotifGenomePostions {
 
 		String wd = "/Users/rnadeau2/Documents/LESMoNlocal/analysis/";
 		
-		String motifFile = wd + "corrNetTop2-400_TPPD_p0.3_coreProteins_h0.4_motifFamiliesInfo.tsv";
+		String motifFile = wd + "corrNetTop2-400_coreTPD_p0.4_coreProteins_h0.7_motifFamiliesInfo.tsv";
 		String fastaFile = wd + "input_files/human_3UTRsequences.txt";
 		String refSeqIdFile = wd + "corrNetTop2_proteinsInNetwork_info.tsv";
 		
-		String positionFilePrefix = wd + "evolutionConservation/TPPD/corrNet2-400_TPPD_p0.3_h0.4_genomicPositions_motif";
-		String bedFilePrefix = wd + "evolutionConservation/TPPD/corrNet2-400_TPPD_p0.3_h0.4_genomicPositions_motif";
+		String positionFilePrefix = wd + "evolutionConservation/coreTPD/corrNet2-400_coreTPD_p0.4_h0.7_genomicPositions_motif";
+		String bedFilePrefix = wd + "evolutionConservation/coreTPD/corrNet2-400_coreTPD_p0.4_h0.7_genomicPositions_motif";
+		
+		String coreProteinAnnotations = wd + "corrNetTop2-400_coreTPD_p0.4_coreProteinsByMotif.tsv";
+		
+		String positionCoreFilePrefix = wd + "evolutionConservation/coreTPD/corrNet2-400_coreTPD_p0.4_h0.7_genomicPositions_core_motif";
+		String bedCoreFilePrefix = wd + "evolutionConservation/coreTPD/corrNet2-400_coreTPD_p0.4_h0.7_genomicPositions_core_motif";
 
-		getGenomePositionsForMotif(motifFile, fastaFile, refSeqIdFile, positionFilePrefix, bedFilePrefix);
+		//getGenomePositionsForMotif(motifFile, fastaFile, refSeqIdFile, positionFilePrefix, bedFilePrefix);
+		
+		getGenomePositionsForMotifFromCoreProteins(motifFile, fastaFile, refSeqIdFile, coreProteinAnnotations, positionCoreFilePrefix, bedCoreFilePrefix);
 	}
 
 	private static void getGenomePositionsForMotif(String motifFile, String fastaFile, String refSeqIdFile, String positionFilePrefix, String bedFilePrefix) {
@@ -53,6 +61,40 @@ public class DetermineMotifGenomePostions {
 
 			/* search FASTA for motif - get GENOME positions */ 
 			HashSet<String> motifPositions = searchForMotifPostions(formattedMotif, fastaFile, refSeqIdFile);
+
+			/* print all positions */
+			printGenomePositionsOfMotif(motifInfo, motifNumber, motifPositions, positionFilePrefix + motifNumber + ".txt");
+
+			/* print BED file */
+			printBedFiles(motifInfo, motifNumber, motifPositions, bedFilePrefix + motifNumber + ".bed");
+		}
+	}
+	
+	private static void getGenomePositionsForMotifFromCoreProteins(String motifFile, String fastaFile, String refSeqIdFile, String coreProteinAnnotations, String positionFilePrefix, String bedFilePrefix) {
+
+		/* get motifs to test  */
+		List<String> motifsToAssess = loadMotifsToAssess(motifFile);
+		
+		/* set character map for regular expression */ 
+		HashMap<Character, String> characterMap = setCharacterMapForRegularExpression();
+
+		/* assess motifs individually */
+		for(int i=0; i<motifsToAssess.size(); i++) {
+			int motifNumber = i+1;
+			System.out.println("Motif: " + motifNumber);
+			
+			String motif = motifsToAssess.get(i);
+			
+			/* load core proteins */ 
+			HashSet<String> coreProteinSet = loadCoreProteins(motif, coreProteinAnnotations);
+
+			/* convert motif to regular expression */ 
+			String formattedMotif = formatMotifWithRegularExpression(motif, characterMap);
+			
+			String motifInfo = motif + "|" + formattedMotif;
+
+			/* search FASTA for motif - get GENOME positions */ 
+			HashSet<String> motifPositions = searchForMotifPostionsFromCore(formattedMotif, fastaFile, refSeqIdFile, coreProteinSet);
 
 			/* print all positions */
 			printGenomePositionsOfMotif(motifInfo, motifNumber, motifPositions, positionFilePrefix + motifNumber + ".txt");
@@ -131,10 +173,13 @@ public class DetermineMotifGenomePostions {
 
 		return formattedMotif;
 	}
-
+	
+	
 	private static HashSet<String> searchForMotifPostions(String formattedMotif, String fastaFile, String refSeqIdFile){
 
 		HashSet<String> motifPositions = new HashSet<>();
+		
+		/* for each gene select (1) RefSeqId to test */
 		HashSet<String> refSeqIds = getRefSeqIds(refSeqIdFile);
 
 		try {
@@ -165,10 +210,12 @@ public class DetermineMotifGenomePostions {
 					//  >hg38_ncbiRefSeq_NM_001276352.2 range=chr1:67092165-67093579 5'pad=0 3'pad=0 strand=- repeatMasking=none					
 					String id = line.split("[\\_\\s++\\.]")[2] + "_"+ line.split("[\\_\\s++\\.]")[3];
 
-					if(refSeqIds.contains(id)) {
-						readSeq = true;
-						header = line;
-						seq = "";
+					if(refSeqIds.contains(id)) { // do not consider alternate chromosomes
+						if(!line.contains("alt") && !line.contains("_fix")) {
+							readSeq = true;
+							header = line;
+							seq = "";
+						}
 					}
 				}
 
@@ -196,9 +243,13 @@ public class DetermineMotifGenomePostions {
 				
 				if(line.split("\t").length > 1) {
 					String[] ids = line.split("\t")[1].split("\\|"); // [0] protein name, [1] list of refSeqIds
-					refSeqIds.addAll(Arrays.asList(ids));	
+					
+					int numMotifs = ids.length;
+					Random rand = new Random();
+					
+					int randomIdx = rand.nextInt(numMotifs); // random number between 0 and numMotifs (excluding upper bound)
+					refSeqIds.add(ids[randomIdx]);	
 				}
-				
 
 				line = in.readLine();
 			}
@@ -288,4 +339,124 @@ public class DetermineMotifGenomePostions {
 		}
 	}
 
+	
+	private static HashSet<String> loadCoreProteins(String motif, String coreProteinAnnotationFile){
+		
+		HashSet<String> coreProteins = new HashSet<>();
+		
+		try {
+			BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(new File(coreProteinAnnotationFile))));			
+
+			String line = in.readLine();
+
+			while(line!=null) {
+
+				String currentMotif = line.split("\t")[0];
+				if(currentMotif.equals(motif)) {
+					coreProteins.addAll(Arrays.asList(line.split("\t")[2].split("\\|")));
+					break;
+				}
+
+				line = in.readLine();
+			}
+
+			in.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return coreProteins;
+	}
+
+	private static HashSet<String> searchForMotifPostionsFromCore(String formattedMotif, String fastaFile, String refSeqIdFile, HashSet<String> coreProteins){
+
+		HashSet<String> motifPositions = new HashSet<>();
+		
+		/* for each gene select (1) RefSeqId to test */
+		HashSet<String> refSeqIds = getCoreRefSeqIds(refSeqIdFile, coreProteins);
+
+		try {
+			BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(new File(fastaFile))));			
+
+			String line = in.readLine();
+
+			boolean readSeq = false; 
+			String seq = "";
+			String header = "";
+
+			while(line!=null) {
+
+				if(readSeq) {
+					seq += line;
+				}
+
+				/* check if sequence is associated to a protein in network */
+				if(line.startsWith(">")) {
+					readSeq = false;
+
+					/* if sequence has been loaded - check for motif in the sequence  */
+					if(!seq.isEmpty()) {
+						List<String> positions = searchSeqForMotif(formattedMotif, seq, header);
+						motifPositions.addAll(positions);
+					}
+
+					//  >hg38_ncbiRefSeq_NM_001276352.2 range=chr1:67092165-67093579 5'pad=0 3'pad=0 strand=- repeatMasking=none					
+					String id = line.split("[\\_\\s++\\.]")[2] + "_"+ line.split("[\\_\\s++\\.]")[3];
+
+					if(refSeqIds.contains(id)) { // do not consider alternate chromosomes
+						if(!line.contains("alt") && !line.contains("_fix")) {
+							readSeq = true;
+							header = line;
+							seq = "";
+						}
+					}
+				}
+
+				line = in.readLine();
+			}
+
+			in.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return motifPositions;
+	}
+	
+	private static HashSet<String> getCoreRefSeqIds(String refSeqIdFile, HashSet<String> coreProteins){
+
+		HashSet<String> refSeqIds = new HashSet<>();
+
+		try {
+			BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(new File(refSeqIdFile))));			
+
+			String line = in.readLine();
+
+			while(line!=null) {
+				
+				String[] col = line.split("\t");
+				
+				if(col.length > 1) {
+					
+					if(coreProteins.contains(col[0])){ // [0] protein name
+						
+						String[] ids = col[1].split("\\|"); // [0] protein name, [1] list of refSeqIds
+						
+						int numMotifs = ids.length;
+						Random rand = new Random();
+						
+						int randomIdx = rand.nextInt(numMotifs); // random number between 0 and numMotifs (excluding upper bound)
+						refSeqIds.add(ids[randomIdx]);	
+					}
+				}
+
+				line = in.readLine();
+			}
+
+			in.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return refSeqIds;	
+	}
 }
