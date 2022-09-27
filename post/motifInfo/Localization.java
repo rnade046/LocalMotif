1e950a0588c13f5883a2a3bb7e5c3ad4fa41cefd
rnade046 <39959339+rnade046@users.noreply.github.com>
2022-09-27 09:26:37 -0400

    Update localization for plotting

--- post/motifInfo/Localization.java
+++ post/motifInfo/Localization.java
@@ -7,21 +7,28 @@ import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 import java.util.HashSet;
 
 public class Localization {
 
 	public static void main(String[] args) {
 
-	String inputLocalizations = "/Users/rnadeau2/Documents/LESMoNlocal/analysis/motifsOfInterest/subcellular_location.tsv";
-	String summarizedLocalizatiosn = "/Users/rnadeau2/Documents/LESMoNlocal/analysis/motifsOfInterest/summarized_locations.tsv";
-	
-	determinePossibleLocalizations(inputLocalizations, summarizedLocalizatiosn);
-		
+		String inputLocalizations = "/Users/rnadeau2/Documents/LESMoNlocal/analysis/motifsOfInterest/subcellular_location.tsv";
+		String summarizedLocalizatiosn = "/Users/rnadeau2/Documents/LESMoNlocal/analysis/motifsOfInterest/summarized_locations.tsv";
+
+		HashSet<String> local = determinePossibleLocalizations(inputLocalizations, summarizedLocalizatiosn);
+		HashSet<String> motifs = loadMotifsToTest("/Users/rnadeau2/Documents/LESMoNlocal/analysis/motifsOfInterest/coreTPD_motifsOfInterest.tsv");
+
+		for(String m :motifs) {
+			String motifFile = "/Users/rnadeau2/Documents/LESMoNlocal/analysis/motifsOfInterest/coreTPD_motifOfInterest_localization_"+ m + ".tsv"; 
+			updateLocalizationCount(motifFile, local, motifFile);
+		}
+
 	}
 
 
-	public static void determinePossibleLocalizations(String inputLocalization, String summarizedLocalizations) {
+	public static HashSet<String> determinePossibleLocalizations(String inputLocalization, String summarizedLocalizations) {
 
 		HashSet<String> localizations = new HashSet<>();
 
@@ -39,7 +46,7 @@ public class Localization {
 				line = in.readLine();
 			}
 			in.close();
-			
+
 			BufferedWriter out = new BufferedWriter(new FileWriter(new File(summarizedLocalizations)));
 			for(String local : localizations) {
 				out.write(local + "\n");
@@ -49,7 +56,73 @@ public class Localization {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		return localizations;
 	}
 
+
+	private static HashSet<String> loadMotifsToTest(String motifsToTestFile){
+
+		HashSet<String> motifsToTest = new HashSet<>();
+
+		try {
+			BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(new File(motifsToTestFile))));
+
+			String line = in.readLine(); // no-header
+
+			while(line!=null) {
+
+				motifsToTest.add(line.split("\t")[1]);
+				line = in.readLine();
+			}
+
+			in.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return motifsToTest;
+	}
 	
+	private static void	updateLocalizationCount(String motifLocalizations, HashSet<String> possibleLocalizations, String updateCount) {
+		
+		HashMap<String, Double> localCount = new HashMap<>();
+		
+		try {
+			BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(new File(motifLocalizations))));
+
+			String line = in.readLine(); 
+			line = in.readLine();
+
+			while(line!=null) {
+
+				String[] col = line.split("\t");
+				localCount.put(col[0], Double.parseDouble(col[1]));
+				line = in.readLine();
+			}
+
+			in.close();
+			
+			
+			BufferedWriter out = new BufferedWriter(new FileWriter(new File(updateCount)));
+			
+			out.write("Localization\tCount\n");
+			for(String local: possibleLocalizations) {
+				if(localCount.containsKey(local)) {
+					out.write(local + "\t" + localCount.get(local) + "\n");
+				} else {
+					out.write(local + "\t0\n");
+				}
+				out.flush();
+			}
+			
+			out.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		
+	
+	
+	}
+
 }
